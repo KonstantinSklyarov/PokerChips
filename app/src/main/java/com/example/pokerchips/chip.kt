d0b49c56db87b4b54909679b57084d11c2cd777c
kabaniery <zyakindaniil@gmail.com>
2023-08-06 18:21:32 +0300

    Анимация фишек (Функция movingToCenter) для создания анимации

--- app/src/main/java/com/example/pokerchips/chip.kt
+++ app/src/main/java/com/example/pokerchips/chip.kt
@@ -1,24 +1,59 @@
 package com.example.pokerchips
 
-import android.graphics.drawable.Drawable
-import android.view.View
 import android.view.ViewGroup
+import android.view.animation.Animation
+import android.view.animation.TranslateAnimation
 import android.widget.ImageButton
 import android.widget.ImageView
-import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 
 //Лошара ты
 class chip(var imageButton: ImageButton, var appCompatActivity: AppCompatActivity, var chipValue: Int) {
-    public var copyImage: ImageView = ImageView(this.appCompatActivity)
-    init {
-        var draw: Drawable = imageButton.drawable.mutate()
-        draw.alpha = draw.alpha
-        copyImage.setImageDrawable(draw)
-        val scale = appCompatActivity.resources.displayMetrics.scaledDensity
-        // Устанавливаем контейнер как контент для активности
-        appCompatActivity.addContentView(copyImage, ViewGroup.LayoutParams((73 * scale).toInt(), (73 * scale).toInt()))
-        //appCompatActivity.setContentView(container)
+    //Значение sp
+    var scale = appCompatActivity.resources.displayMetrics.scaledDensity
+    //Высота экрана
+    var height = appCompatActivity.resources.displayMetrics.heightPixels
+    //Координата левого края полоски с фишками
+    var xCoord = appCompatActivity.resources.displayMetrics.widthPixels - 376*scale
+
+    //Получение нового изображения фишки в месте нахождения оригинала
+    fun getChip() : ImageView {
+        val newImage = ImageView(this.appCompatActivity)
+        newImage.setImageDrawable(imageButton.drawable.mutate())
+        when (chipValue) {
+            5 -> newImage.x = xCoord
+            10 -> newImage.x = xCoord + (73 * scale)
+            25 -> newImage.x = xCoord + (73 * scale) * 2
+            50 -> newImage.x = xCoord + (73 * scale) * 3
+            100 -> newImage.x = xCoord + (73 * scale) * 4
+        }
+        newImage.y = height - (16 + 80 + 5 + 75) * scale
+        newImage.requestLayout()
+        appCompatActivity.addContentView(newImage, ViewGroup.LayoutParams((73 * scale).toInt(), (73 * scale).toInt()))
+        return newImage
+    }
+    //Анимация движения фишки к центру
+    fun movingToCenter() {
+        val imageView = getChip()
+        //Координаты конечного местоположения
+        val xCenter = appCompatActivity.resources.displayMetrics.widthPixels / 2f - 30 * scale
+        val yCenter = height / 2 - 25 * scale
+        //Сколько длится анимация
+        val animDur = 1000L
+        val animation = TranslateAnimation((-19 * scale), xCenter - imageView.x, (-20 * scale), yCenter - imageView.y).apply {
+            duration = animDur
+        }
+        animation.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationStart(animation: Animation?) {}
+
+            override fun onAnimationEnd(animation: Animation?) {
+                val parent = imageView.parent as? ViewGroup
+                parent?.removeView(imageView)
+            }
+
+            override fun onAnimationRepeat(animation: Animation?) {}
+        })
+        imageView.startAnimation(animation)
 
     }
 
